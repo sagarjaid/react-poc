dbd2c41bfe55646c415d2c4da302ca81f33a0e80
sagarjaid <69151227+sagarjaid@users.noreply.github.com>
2023-01-05 15:53:52 +0530

    added empty state

--- src/hooks/useSearch.jsx
+++ src/hooks/useSearch.jsx
@@ -3,10 +3,9 @@ import useGetData from './useGetData';
 
 const useSearch = (url) => {
   const [searchTerm, setSearchTerm] = useState(null);
-  const [key, setKey] = useState("id");
+  const [key, setKey] = useState('');
 
-
-  const {Data, loading, getData, setData} = useGetData(url);
+  const { Data, loading, getData, setData } = useGetData(url);
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
@@ -14,10 +13,9 @@ const useSearch = (url) => {
     if (e.target.value == '' || null) getData(url);
 
     const results = Data.filter((el) => {
-
-      let keyz = el.hasOwnProperty(key)
-      console.log(keyz, "keyz");
-      if(keyz && key){
+      let keyz = el.hasOwnProperty(key);
+      console.log(keyz, 'keyz');
+      if (keyz && key) {
         return el[key]?.toLowerCase().includes(e.target.value.toLowerCase());
       }
     });
@@ -25,7 +23,15 @@ const useSearch = (url) => {
     setData(results);
   };
 
-  return { searchTerm, loading, Data, setSearchTerm, handleSearch, setKey, url };
+  return {
+    searchTerm,
+    loading,
+    Data,
+    setSearchTerm,
+    handleSearch,
+    setKey,
+    url,
+  };
 };
 
 export default useSearch;
